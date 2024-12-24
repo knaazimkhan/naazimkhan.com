f21ef421c99019a809d1b164912478c6492591c3
Naazim Khan <knaazimkhan@gmail.com>
2024-12-24 15:00:21 +0400

    all rm

--- src/containers/index.js
+++
@@ -1,3 +0,0 @@
-export { default as LeftSidebar } from './leftsidebar/LeftSidebar';
-export { default as Main } from './main/Main';
-export { default as RightSidebar } from './rightsidebar/RightSidebar';
